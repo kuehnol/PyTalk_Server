0a4af81a36c91ce2687d69df09bbd10cbf54ae31
valiente <val@ien.te>
2024-03-10 21:10:43 +0100

    removed TODO

--- main.py
+++ main.py
@@ -12,6 +12,3 @@ db = UserDB()
 if __name__ == "__main__":
     server = Server(output_obj=output, db_obj=db)
     server.start(port=PORT, max_connections=MAX_CONNECTIONS)
-
-# TODO: Use logging instead of terminal_out (maybe)
-#       Use JSON for network transmissions, send everything in one go
